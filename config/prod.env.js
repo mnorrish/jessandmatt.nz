48dff52f5f60dcb1f4966270127847580db4a515
Matthew Norrish <matt.norrish@clavisinsight.com>
2017-03-25 14:02:21 +1300

    add Google Analytics tracking

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,3 +1,4 @@
 module.exports = {
-  NODE_ENV: '"production"'
+  NODE_ENV: '"production"',
+  googleAnalyticsTrackingID: 'UA-96158118-1',
 }
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
+import ga from 'vue-ga';
 import Home from 'components/Home';
 import Location from 'components/Location';
 import Accommodation from 'components/Accommodation';
@@ -8,7 +9,7 @@ import RSVP from 'components/RSVP';
 
 Vue.use(Router);
 
-export default new Router({
+const router = new Router({
   routes: [
     {
       path: '/',
@@ -37,3 +38,10 @@ export default new Router({
     },
   ],
 });
+
+// track routes only in production build
+if (process.env.NODE_ENV === 'production') {
+  ga(router, process.env.googleAnalyticsTrackingID);
+}
+
+export default router;
