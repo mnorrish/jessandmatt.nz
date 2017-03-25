ae788e84d10b2b8b473955dbfe37ad07fd029531
Matthew Norrish <matt.norrish@clavisinsight.com>
2017-03-25 13:44:43 +1300

    add CNAME file generation

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -6,6 +6,7 @@ var merge = require('webpack-merge')
 var baseWebpackConfig = require('./webpack.base.conf')
 var HtmlWebpackPlugin = require('html-webpack-plugin')
 var ExtractTextPlugin = require('extract-text-webpack-plugin')
+var cnamePlugin = require('cname-webpack-plugin');
 var env = config.build.env
 
 var webpackConfig = merge(baseWebpackConfig, {
@@ -71,6 +72,9 @@ var webpackConfig = merge(baseWebpackConfig, {
     new webpack.optimize.CommonsChunkPlugin({
       name: 'manifest',
       chunks: ['vendor']
+    }),
+    new cnamePlugin({
+      domain: config.build.cname
     })
   ]
 })
